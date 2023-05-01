032263137ac422b24f00a6ebbedbd4e94d11a2bb
Konstantin Fomenkov <kolij2005@gmail.com>
2023-05-01 07:36:53 +0300

    Initial Commit

---
+++ LoginApp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginApp2
+//
+//  Created by Konstantin Fomenkov on 01.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
